d096b2ffaf233886dda41ad321c7f194a2cd08ad
SoumyaSinghcodes <soumyasinghthakur147@gmail.com>
2024-08-02 23:17:16 +0530

    added moment.js timezones for two countries

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+setInterval(function () {
+  //LA
 
+  let LAElement = document.querySelector("#LA");
+  let LADateElement = LAElement.querySelector(".date");
+  let LATimeElement = LAElement.querySelector(".time");
+  let LATime = moment().tz("America/Los_Angeles");
+
+  LADateElement.innerHTML = LATime.format("MMMM Do YYYY");
+  LATimeElement.innerHTML = LATime.format("h:mm:ss  [<small>]A[</small>]");
+}, 1000);
+
+setInterval(function () {
+  //PA
+
+  let SYElement = document.querySelector("#SY");
+  let SYDateElement = SYElement.querySelector(".date");
+  let SYTimeElement = SYElement.querySelector(".time");
+  let SYTime = moment().tz("Australia/Sydney");
+
+  SYDateElement.innerHTML = SYTime.format("MMMM Do YYYY");
+  SYTimeElement.innerHTML = SYTime.format("h:mm:ss  [<small>]A[</small>]");
+}, 1000);
